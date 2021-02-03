3b43a6a38d5308bcfb68baf5b409ba4b59efc8f3
caizunguang <android@gdong.net>
2021-02-03 15:45:41 +0800

    gif supported test

--- app/src/main/java/com/example/richtextview/MainActivity.java
+++ app/src/main/java/com/example/richtextview/MainActivity.java
@@ -1,34 +1,60 @@
 package com.example.richtextview;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
+import android.text.style.ImageSpan;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.gifdecoder.StandardGifDecoder;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.czg.richtextview.MyHtml;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView ivImage;
+    private List<Bitmap> bitmaps = new ArrayList<>();
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView  textView = findViewById(R.id.text);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        mTextView = findViewById(R.id.text);
+        ivImage = findViewById(R.id.iv_img);
+        mTextView.setMovementMethod(LinkMovementMethod.getInstance());
         String src = "<div id=\"se-knowledge\"><p><big>H</big>ello <i>world!</i></p>"
                 + "<img src=\"https://t7.baidu.com/it/u=1595072465,3644073269&fm=193&f=GIF\"/>"
                 + "<ul>" +
@@ -39,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 "</ul>"
                 + "successful!"
                 + "</div>";
-        MyHtml.init(textView);
+        MyHtml.init(mTextView);
         Spanned spanned = MyHtml.fromHtml(src, MyHtml.FROM_HTML_MODE_COMPACT, new MyHtml.ImageGetter() {
             @Override
             public Drawable getDrawable(String source, int start) {
@@ -47,17 +73,58 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
         }, null);
-        textView.setText(spanned);
+        mTextView.setText(spanned);
     }
 
     public void getImage(String source, int start) {
         if (TextUtils.isEmpty(source)) return;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                request(source, start);
-            }
-        }).start();
+        Handler handler = new Handler();
+        Glide.with(this)
+                .asGif()
+                .load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201611%2F04%2F20161104110413_XzVAk.thumb.700_0.gif&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614922677&t=90bd10eb2a248d80084eec919f8ab23a")
+                .into(new CustomTarget<GifDrawable>() {
+                    @Override
+                    public void onResourceReady(@NonNull GifDrawable resource, @Nullable Transition<? super GifDrawable> transition) {
+                        try {
+                            Object GifState = resource.getConstantState();
+                            Field frameLoader = GifState.getClass().getDeclaredField("frameLoader");
+                            frameLoader.setAccessible(true);
+                            Object gifFrameLoader = frameLoader.get(GifState);
+
+                            Field gifDecoder = gifFrameLoader.getClass().getDeclaredField("gifDecoder");
+                            gifDecoder.setAccessible(true);
+                            StandardGifDecoder standardGifDecoder = (StandardGifDecoder) gifDecoder.get(gifFrameLoader);
+
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (mTextView.getText().length() > start) {
+                                        standardGifDecoder.advance();
+                                        SpannableString spannableString = (SpannableString) mTextView.getText();
+                                        spannableString.setSpan(new ImageSpan(MyHtml.sContext, standardGifDecoder.getNextFrame()), start, start + 1,
+                                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                        handler.postDelayed(this, standardGifDecoder.getNextDelay());
+                                    }
+                                }
+                            });
+
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
+//    new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                request(source, start);
+//            }
+//        }).start();
 
     }
 
